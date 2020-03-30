51a44697eb4e4faad20c8746d18fb83a027d32d7
JesusPadilla <padillaultragenerationx@gmail.com>
2020-03-30 10:24:04 -0700

    Se modifica platilla basica y se añade un contador

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -2,46 +2,35 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//function Hello (props) {
-//    return <h2>{props.title}</h2>
-//}
+class Contador extends Component {
+    constructor() {
+        super()
+        this.state = { contador: 1}
+        setInterval(() => {
+            this.setState({ contador: this.state.contador + 1})
+        }, 1000)
 
-class Hello extends Component{
-    render(){
-        return <h2>{this.props.title}</h2>
+    }
+    render() {
+    return <span>{this.state.contador}</span>
     }
 }
 
-class Text extends Component{
+class ContadorNumero extends Component{
     render(){
-        return(
-        <div>
-            <p>{this.props.Text}</p>
-            <p>{this.props.number}</p>
-            <p>{this.props.boolean}</p>
-        </div>
+        return <span>{this.props.numero}</span>
+    }
+}
+
+class App extends Component{
+    render(){
+        return (
+            <div className="App">
+                <p>Primer Componente con state</p>
+                <Contador />
+            </div>   
         )
-         
     }
-}
-
-function App() {
-    return (
-        <div className = "App" >
-        <header className = "App-header" >
-        <img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <Hello title= "Hello from props"/>
-        <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React 
-        </a>
-        </header> <
-        /div>
-    );
 }
 
 export default App;
